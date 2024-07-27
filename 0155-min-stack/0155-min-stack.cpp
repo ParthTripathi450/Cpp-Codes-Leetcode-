43e606cc14692cf7550f97b02d0097dcf338f4c6
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2024-07-27 11:11:59 +0530

    Time: 15 ms (77.54%), Space: 21.7 MB (21.02%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,29 +1,50 @@
 class MinStack {
 private:
-    stack<pair<int,int>> stk; 
+    stack <long long> stk;
+    long long min=INT_MIN;
 public:
     MinStack() {
         
     }
     
     void push(int val) {
-        if(stk.empty()) stk.push({val,val});
-        else{
-            stk.push({val,min(val,stk.top().second)});
+        long long value = val;
+        if(stk.empty()){
+            stk.push(value);
+            min = value;
         }
-        
+        else{
+            if(value<min){
+                stk.push(2*value - min);
+                min = value;
+            }
+            else{
+                stk.push(value);
+            }
+        }
     }
     
     void pop() {
-        stk.pop();
+        if (stk.empty()) return;
+        if(min > stk.top()){
+            min = 2*min - stk.top();
+            stk.pop();
+        }
+        else{
+            stk.pop();
+        }
     }
     
     int top() {
-        return stk.top().first;
+        if(stk.empty()) return -1;
+        if(stk.top()<min) return min;
+        else{
+            return stk.top();
+        }
     }
     
     int getMin() {
-        return stk.top().second;
+        return min;
     }
 };
 
