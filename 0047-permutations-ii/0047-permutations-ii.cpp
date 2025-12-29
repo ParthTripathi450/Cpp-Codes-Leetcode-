b8d64b1d7691a8931075e82925e068f225d43744
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2025-12-30 02:17:35 +0530

    Time: 3 ms (65.16%), Space: 11.9 MB (60.29%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> curr;
+    vector<bool> used;
+
+    void dfs(vector<int>& nums){
+        if(curr.size() == nums.size()){
+            ans.push_back(curr);
+            return;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(used[i])continue;
+            if(i>0 && nums[i] == nums[i-1]  && !used[i-1])continue;
+            curr.push_back(nums[i]);
+            used[i] = true;
+
+            dfs(nums);
+
+            curr.pop_back();
+            used[i] = false;
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        used.resize(nums.size(), false);
+        dfs(nums);
+        return ans;
+    }
+};
