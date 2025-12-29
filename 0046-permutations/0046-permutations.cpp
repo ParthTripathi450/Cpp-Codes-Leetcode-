44e0d38c2c112a5710e29310a6b0248bb95bdc68
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2025-12-30 01:17:58 +0530

    Time: 3 ms (28.52%), Space: 11 MB (28.65%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    void func(int start,vector<int>& nums,vector<vector<int>>& res){
-        if(start == nums.size()){
-            res.push_back(nums);
+    vector<vector<int>> ans;
+    vector<int> curr;
+    vector<bool> used;
+    void dfs(vector<int>& nums){
+        if(nums.size() == curr.size()){
+            ans.push_back(curr);
             return;
         }
-        for(int i=start;i<nums.size();i++){
-            swap(nums[i],nums[start]);
-            func(start+1,nums,res);
-            swap(nums[i],nums[start]);
-        }
-    } 
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> res;
-        //vector<int> set;
-        func(0,nums,res);
-        return res;
+        for(int i=0;i<nums.size();i++){
+            if(used[i])continue;
+            used[i] = true;
+            curr.push_back(nums[i]);
+            dfs(nums);
 
+            curr.pop_back();
+            used[i] = false;
+
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        used.resize(nums.size(), false);
+        dfs(nums);
+        return ans;
     }
 };
