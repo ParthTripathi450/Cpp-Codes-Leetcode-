0b277a1e3c01cbd57727bed71d2c5c854b42b30e
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2025-12-29 22:13:16 +0530

    Time: 72 ms (35.31%), Space: 82.6 MB (31.18%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> curr;
+    void dfs(int start,int n,int k){
+        if(curr.size() == k){
+            ans.push_back(curr);
+            return;
+        }
+        for(int i=start;i<=n;i++){
+            curr.push_back(i);
+            dfs(i+1,n,k);
+            curr.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        dfs(1,n,k);
+        return ans;
+    }
+};
