502e768d8debf71cafa4ea3d02dcefe19221f18f
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2025-12-30 01:43:16 +0530

    Time: 3 ms (39.7%), Space: 14.3 MB (28.6%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        vector <vector <int>> ans;
-        vector <int> v;
-        solution(0,target,candidates,ans,v);
-        return ans;
-    }
-    void solution(int i,int target,vector <int> &candidates,vector <vector <int>> &ans,vector <int> &v){
-        if(target==0){
-           ans.push_back(v);
+    vector<int> curr;
+    vector<vector<int>> ans;
+    void dfs(vector<int>& candidates, int target,int start) {
+        if(target == 0){
+            ans.push_back(curr);
             return;
         }
-        for(int index=i;index<candidates.size();index++){
-            if(index>i && candidates[index]==candidates[index-1]) continue;
-            if(candidates[index]>target) break;
-            v.push_back(candidates[index]);
-            solution(index+1,target-candidates[index],candidates,ans,v);
-            v.pop_back();
+        if(target<0){
+            return;
+        }   
+        for(int i=start;i<candidates.size();i++){
+            if (i > start && candidates[i] == candidates[i - 1]) continue;
+            curr.push_back(candidates[i]);
+            dfs(candidates,target-candidates[i],i+1);
+            curr.pop_back();
         }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target){
+        sort(candidates.begin(), candidates.end());
+        dfs(candidates,target,0);
+        return ans;
     }
 };
