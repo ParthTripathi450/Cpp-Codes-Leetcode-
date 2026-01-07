6699443a6cc6cd7103e7433e6204d9d04350ecd3
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2026-01-07 06:17:01 +0530

    Time: 4 ms (25.71%), Space: 14.4 MB (20.34%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-    int find(vector<int>&parent,int u){
-        if(u!=parent[u]){
-            parent[u] = find(parent,parent[u]);
-        }
-        return parent[u];
-    }
-    bool unionSet(int u,int v,vector<int>&parent,vector<int>&rank){
-        int pu = find(parent,u);
-        int pv = find(parent,v);
-        if(pu==pv)return false;
-        if(rank[pu]>rank[pv]){
-            parent[pv] = pu;
-        }else if(rank[pv]>rank[pu]){
-            parent[pu] = pv;
-        }else{
-            parent[pu] = pv;
-            rank[pv]++;
-        }
-        return true;
-    }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        vector<int> rank(n+1,0);
-        vector<int> parent(n+1);
-        for(int i=1;i<=n;i++)parent[i] = i;
-        for(auto &edge:edges){
-            int u = edge[0];
-            int v = edge[1];
-            if(!unionSet(u,v,parent,rank)){
-                return edge;
+        vector<vector<int>> adj(n+1);
+
+        for(const auto& edge :edges){
+            int u = edge[0],v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+            vector<bool> visit(n+1,false);
+
+            if(dfs(u,-1,adj,visit)){
+                return {u,v};
             }
         }
         return {};
+    }
+private:
+    bool dfs(int node, int parent,vector<vector<int>>&adj, vector<bool>&visit){
+        if(visit[node]) return true;
+        visit[node] = true;
+        for(int nei:adj[node]){
+            if(nei == parent) continue;
+            if(dfs(nei,node,adj,visit)) return true;
+        }
+        return false;
     }
 };
