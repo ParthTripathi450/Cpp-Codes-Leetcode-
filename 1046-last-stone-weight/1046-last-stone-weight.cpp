f9931a9f82f699f011f275d4579d25cd89650e92
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2026-01-11 03:20:09 +0530

    Time: 0 ms (100%), Space: 10 MB (45.15%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        while(stones.size()>1){
+            sort(stones.begin(),stones.end());
+            int curr = stones.back() - stones[stones.size()-2];
+            stones.pop_back();
+            stones.pop_back();
+            if(curr!=0){
+                stones.push_back(curr);
+            }
+        }
+        return stones.empty() ? 0 : stones[0];
+    }
+};
