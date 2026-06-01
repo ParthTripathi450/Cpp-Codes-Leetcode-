c2c92a8abc100687bb6f1a21263735683e24a831
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2026-06-02 02:13:03 +0530

    Time: 432 ms (21.82%), Space: 11.7 MB (84.35%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int solve(int ind, int target, vector<int>& nums) {
+        if (ind==0){
+            if (target == 0 && nums[0] == 0) return 2;
+            if (target == nums[0]||target== -nums[0]) return 1;
+            return 0;
+        }
+        int plus =solve(ind-1, target-nums[ind],nums);
+        int minus =solve(ind-1, target + nums[ind],nums);
+        return plus+minus;
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return solve(nums.size()-1,target,nums);
+    }
+};
