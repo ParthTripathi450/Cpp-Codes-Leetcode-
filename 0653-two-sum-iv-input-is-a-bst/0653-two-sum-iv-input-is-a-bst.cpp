074c7b691e19cc13b174c826573d8d562584fd51
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2026-06-07 00:02:18 +0530

    Time: 17 ms (6.11%), Space: 39 MB (15%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -11,27 +11,16 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int>& nums){
-        if(!root)return;
-
-        inorder(root->left,nums);
-        nums.push_back(root->val);
-        inorder(root->right,nums);
+    unordered_set<int> st;
+    bool dfs(TreeNode* root,int k) {
+        if (!root) return false;
+        if (st.count(k -root->val))
+            return true;
+        st.insert(root->val);
+        return dfs(root->left,k) ||
+               dfs(root->right,k);
     }
-    bool findTarget(TreeNode* root, int k) {
-        vector<int> nums;
-        inorder(root,nums);
-
-        int l =0,r=nums.size()-1;
-
-        while(l<r){
-            int sum = nums[l] + nums[r];
-
-            if(sum == k) return true;
-            if(sum<k)l++;
-            else r--;
-        }
-        return false;
+    bool findTarget(TreeNode* root,int k) {
+        return dfs(root, k);
     }
-
 };
