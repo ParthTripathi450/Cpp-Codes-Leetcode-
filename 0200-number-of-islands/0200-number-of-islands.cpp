27d03d51d8e25fe0597e3da1ae9026b433685a82
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2026-06-18 01:28:00 +0530

    Time: 26 ms (62.94%), Space: 16.2 MB (78.49%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    int directions[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
+    void dfs(int r,int c,vector<vector<char>>& grid){
+        int m = grid.size();
+        int n = grid[0].size();
+        if(r<0 || r>=m || c<0 || c >= n || grid[r][c] =='0')return;
+        grid[r][c] = '0';
+
+        dfs(r-1,c,grid);
+        dfs(r+1,c,grid);
+        dfs(r,c-1,grid);
+        dfs(r,c+1,grid);
+    }
 
     int numIslands(vector<vector<char>>& grid) {
-        int rows = grid.size(), cols = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
+
         int islands = 0;
-        for(int r=0;r<grid.size();r++){
-            for(int c=0;c<grid[0].size();c++){
-                if(grid[r][c] == '1'){
-                    dfs(grid,r,c);
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+
+                if(grid[i][j] == '1'){
                     islands++;
+                    dfs(i,j,grid);
                 }
             }
         }
         return islands;
-    }
-    void dfs(vector<vector<char>>&grid,int r,int c){
-        if(r<0 || c<0 || r>=grid.size() || c>=grid[0].size() || grid[r][c]=='0')return;
-        grid[r][c] = '0';
-        for(int i=0;i<4;i++){
-            dfs(grid,r+directions[i][0],c+directions[i][1]);
-        }
     }
 };
