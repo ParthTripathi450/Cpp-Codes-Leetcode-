1311cb89a6432dab47fa56381ec1750150a37dcc
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2026-07-26 15:52:28 +0530

    Time: 0 ms (100%), Space: 17.8 MB (23.97%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int res = nums[0];
-        for(int i=0;i<nums.size();i++){
-            int cur = nums[i];
-            res = max(res,cur);
-            for(int j = i+1;j<nums.size();j++){
-                cur*=nums[j];
-                res = max(res,cur);
-            }
+        int curMin = 1, curMax = 1;
+
+        for (int num : nums) {
+            int tmp = curMax * num;
+            curMax = max(max(tmp, num * curMin), num);
+            curMin = min(min(tmp, num * curMin), num);
+            res = max(res, curMax);
         }
         return res;
     }
