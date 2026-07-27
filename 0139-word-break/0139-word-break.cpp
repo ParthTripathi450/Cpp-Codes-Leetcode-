d646d63de3f0652420f98f7c3e488c419153102d
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2026-07-27 22:01:39 +0530

    Time: 0 ms (100%), Space: 10.3 MB (98.4%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    unordered_map<int,bool> memo;
     bool wordBreak(string s, vector<string>& wordDict) {
-        memo[s.length()] = true;
-        return dfs(s,wordDict,0);
-    }
-    bool dfs(string& s,vector<string>&wordDict,int i){
-        if(memo.find(i) != memo.end()){
-            return memo[i];
-        }
-        for(const string& w: wordDict){
-            if(i + w.length() <= s.length() && s.substr(i,w.length()) == w){
-                if(dfs(s,wordDict,i+w.length())){
-                    memo[i] = true;
-                    return true;
+        vector<bool> dp(s.size()+1,false);
+        dp[s.size()] = true;
+
+        for(int i = s.size()-1;i>=0;i--){
+            for(const auto &word: wordDict){
+                if((i+word.length()<=s.length()) &&
+                s.substr(i,word.length()) == word){
+                    dp[i] = dp[i + word.length()];
+                }
+                if(dp[i]){
+                    break;
                 }
             }
         }
-        memo[i] = false;
-        return false;
+        return dp[0];
     }
 };
