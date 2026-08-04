80e3b39aa9160dd29eaf9a626546f4466955fe5b
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2026-08-04 22:43:08 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.75%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    vector<int> cache;
-    int dfs(int n,int i){
-        if(i>=n)return i == n;
-        if(cache[i]!= -1)return cache[i];
-        return cache[i] = dfs(n,i+1) + dfs(n,i+2); 
+    int dfs(int n,int i,vector<int>& cache){
+        if(i==n)return 1;
+        if(i>n)return 0;
+        if(cache[i]!=-1)return cache[i];
+
+        return cache[i] = dfs(n,i+1,cache) + dfs(n,i+2,cache);
     }
     int climbStairs(int n) {
+        vector<int> cache;
         cache.resize(n,-1);
-        return dfs(n,0);
+        return dfs(n,0,cache);
     }
 };
