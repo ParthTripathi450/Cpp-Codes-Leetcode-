5ff49d118d95919d899d85015dfa59f4605c277e
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2026-09-02 16:47:42 +0530

    Time: 1662 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,15 +1,15 @@
 # Write your MySQL query statement below
-SELECT p.product_id,
-       COALESCE(
-           (
-               SELECT p2.new_price
-               FROM Products p2
-               WHERE p2.product_id = p.product_id
-                 AND p2.change_date <= '2019-08-16'
-               ORDER BY p2.change_date DESC
-               LIMIT 1
-           ),
-           10
-       ) AS price
-FROM Products p
-GROUP BY p.product_id;
+select p1.product_id,
+coalesce(
+    (
+        select p2.new_price
+        from products p2
+        where p1.product_id = p2.product_id
+        and change_date <= '2019-08-16'
+        order by p2.change_date DESC
+        limit 1
+    )
+    ,10
+) as price
+from products p1
+group by p1.product_id;
