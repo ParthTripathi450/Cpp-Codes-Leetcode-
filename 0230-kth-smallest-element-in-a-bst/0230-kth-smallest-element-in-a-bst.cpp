ac047c81fa415e0d0d85839ba35044b429d741d4
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2026-09-08 23:11:25 +0530

    Time: 0 ms (100%), Space: 24.3 MB (91.2%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,33 +11,20 @@
  */
 class Solution {
 public:
-    int ans, count = 0;
-    void inorder(TreeNode* root, int k){
+    int ans;
+    int cnt=0;
+    void inorder(TreeNode* root,int k){
         if(!root)return;
         inorder(root->left,k);
-        if(++count == k){
+        cnt++;
+        if(cnt == k){
             ans = root->val;
             return;
         }
         inorder(root->right,k);
     }
-    int kthSmallest(TreeNode* root, int k){
+    int kthSmallest(TreeNode* root, int k) {
         inorder(root,k);
         return ans;
     }
-    // int kthSmallest(TreeNode* root, int k) {
-    //     stack<TreeNode*> st;
-    //     TreeNode* curr = root;
-    //     while(curr!=nullptr || !st.empty()){
-    //         while(curr != nullptr){
-    //             st.push(curr);
-    //             curr = curr->left;
-    //         }
-    //         curr = st.top();
-    //         st.pop();
-    //         if(--k == 0)return curr->val;
-    //         curr = curr->right;
-    //     }
-    //     return -1;
-    // }
 };
