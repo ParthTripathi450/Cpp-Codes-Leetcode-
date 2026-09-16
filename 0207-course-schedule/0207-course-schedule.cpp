c6b0d6748fc57a9aabc41edcb1a0fbc6c022c452
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2026-09-16 23:14:40 +0530

    Time: 11 ms (21.67%), Space: 20.3 MB (17.29%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,28 +1,35 @@
 class Solution {
-    unordered_map<int,vector<int>> preMap;
-    unordered_set<int> visiting;
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        for(int i=0;i<numCourses;i++){
-            preMap[i] = {};
+        int n = numCourses;
+        vector<vector<int>> adj(n);
+
+        for(auto it : prerequisites) {
+            adj[it[1]].push_back(it[0]);
         }
-        for(const auto& prereq : prerequisites ){
-            preMap[prereq[0]].push_back(prereq[1]);
+        vector<int> indegree(n,0);
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
         }
-        for(int c=0;c<numCourses;c++){
-            if(!dfs(c))return false;
+        for(int i=0;i<n;i++){
+            if(indegree[i] == 0)q.push(i);
         }
-        return true;
-    }
-    bool dfs(int crs){
-        if(visiting.count(crs))return false;
-        if(preMap[crs].empty())return true;
-        visiting.insert(crs);
-        for(int pre : preMap[crs]){
-            if(!dfs(pre))return false;
+    
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            topo.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0)q.push(it);
+            }
         }
-        visiting.erase(crs);
-        preMap[crs].clear();
-        return true;
+        if(topo.size() == n)return true;
+        return false;
     }
 };
