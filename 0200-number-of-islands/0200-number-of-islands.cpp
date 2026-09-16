a16636d89969b597bb017da398ba3fd066aa8aac
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2026-09-16 16:34:19 +0530

    Time: 27 ms (55.03%), Space: 23.3 MB (28.2%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,11 +3,27 @@ public:
     int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
     int ROWS;
     int COLS;
-    void dfs(vector<vector<char>>& grid,int i,int j){
-        if(i<0 || i>=ROWS ||j<0 || j>=COLS || grid[i][j] == '0' )return;
+
+    void bfs(vector<vector<char>>& grid,int i,int j){
+        queue<pair<int,int>> q;
+        q.push({i,j});
         grid[i][j] = '0';
-        for(int k=0;k<4;k++){
-            dfs(grid,i +dir[k][0],j + dir[k][1]);
+        while(!q.empty()){
+            auto [x,y] = q.front();
+            q.pop();
+
+            for(int k=0;k<4;k++){
+                int nx = x + dir[k][0];
+                int ny = y + dir[k][1];
+
+                if(nx>= 0 && nx <ROWS &&
+                   ny>=0  && ny <COLS &&
+                   grid[nx][ny]=='1'){
+                    q.push({nx,ny});
+                    grid[nx][ny] = '0';
+                }
+            }
+
         }
     }
     int numIslands(vector<vector<char>>& grid) {
@@ -17,7 +33,7 @@ public:
         for(int i=0;i<ROWS;i++){
             for(int j=0;j<COLS;j++){
                 if(grid[i][j] == '1'){
-                    dfs(grid,i,j);
+                    bfs(grid,i,j);
                     islands++;
                 }
             }
