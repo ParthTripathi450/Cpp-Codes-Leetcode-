19f8d9aed99d74ba8ddf9aa1d4b9bfdd457ceb37
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2026-09-17 18:27:22 +0530

    Time: 0 ms (100%), Space: 31.3 MB (81.08%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -3,14 +3,14 @@ public:
     int search(vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size()-1;
+        int mid;
         while(left<=right){
-            int mid = (left+right)/2;
-            if(nums[mid]==target)return mid;
-            else if(nums[mid]>target){
-                right = mid-1;
-            }
-            else{
+            mid = left + ((right-left)/2);
+            if(nums[mid] == target)return mid;
+            if(nums[mid]<target){
                 left = mid+1;
+            }else{
+                right = mid-1;
             }
         }
         return -1;
